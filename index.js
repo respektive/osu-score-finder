30e04fc77ad09fb569991eeabf7c6f044f005d2f
WitchOfFrost <unlimited.gamer@gmx.de>
2020-07-19 21:55:43 +0200

    Improved Error Logging.

--- index.js
+++ index.js
@@ -8,70 +8,100 @@ const module_index = require('./module_index.js');
 const config = require('./config.json');
 const api_key = require('./api_key.json');
 
+// Other Variables
+let data = {}
+
 //File Reset
 fs.writeFile("logs.txt", "", function (err) {
     if (err) {
         logs = ({
-            data: `[ERROR] | ${new Date(Date.now()).toLocaleString()} | ${err}\n`,
+            data: err,
+            type: "obj",
             level: 3
         });
         module_index.logging.main(logs)
     }
 });
 
-// User Prompt
+async function wrapper() {
 
-let prompt_schema = {
-    properties: {
-        mode: {
-            description: 'Select Mode you want to Search',
-            default: 0,
-            pattern: /^[0-3]{1}$/,
-            message: '\nMode has to be a single digit between 0 and 3.\n0 = osu!, 1 = Taiko, 2 = CtB, 3 = osu!mania',
-            required: true
-        },
-        user_id: {
-            description: 'Enter User-ID you want to Search',
-            pattern: /^[0-9]+$/,
-            message: '\nUser ID has to be numeric.',
-            required: true
-        },
-        start_date: {
-            description: 'Enter the date you want to start your Search with',
-            pattern: /^[2][0-9]{3}[-][0-1][1-9][-][0-3][0-9]$/,
-            message: '\nHas to be a valid date! Has to be in format: YYYY-MM-DD',
-            required: true
-        },
-        end_date: {
-            description: 'Enter the date you want to end your Search with',
-            pattern: /^[2][0-9]{3}[-][0-1][1-9][-][0-3][0-9]$/,
-            message: '\nHas to be a valid date! Has to be in format: YYYY-MM-DD',
-            required: true
-        },
-        converts: {
-            description: 'Do you want to include converts? !! WARNING LONG PROCESSING !! 0 = NO, 1 = YES',
-            pattern: /^[0-1]$/,
-            message: '\nHas to be a valid date! Has to be in format: YYYY-MM-DD',
-            required: true
+    // User Prompt
+
+    let prompt_schema = {
+        properties: {
+            mode: {
+                description: 'Select Mode you want to Search',
+                default: 0,
+                pattern: /^[0-3]{1}$/,
+                message: '\nMode has to be a single digit between 0 and 3.\n0 = osu!, 1 = Taiko, 2 = CtB, 3 = osu!mania',
+                required: true
+            },
+            user_id: {
+                description: 'Enter User-ID you want to Search',
+                pattern: /^[0-9]+$/,
+                message: '\nUser ID has to be numeric.',
+                required: true
+            },
+            start_date: {
+                description: 'Enter the date you want to start your Search with',
+                pattern: /^[2][0-9]{3}[-][0-1][1-9][-][0-3][0-9]$/,
+                message: '\nHas to be a valid date! Has to be in format: YYYY-MM-DD',
+                required: true
+            },
+            end_date: {
+                description: 'Enter the date you want to end your Search with',
+                pattern: /^[2][0-9]{3}[-][0-1][1-9][-][0-3][0-9]$/,
+                message: '\nHas to be a valid date! Has to be in format: YYYY-MM-DD',
+                required: true
+            },
+            converts: {
+                description: 'Do you want to include converts? !! WARNING LONG PROCESSING !! 0 = NO, 1 = YES',
+                pattern: /^[0-1]$/,
+                message: '\nHas to be a valid date! Has to be in format: YYYY-MM-DD',
+                required: true
+            }
         }
     }
-}
 
-prompt.start();
-console.log(`osu! score finder v${config.version}`);
-prompt.get(prompt_schema, function (err, result) {
-    if (err) {
-        logs = ({
-            data: `[ERROR] | ${new Date(Date.now()).toLocaleString()} | ${err}\n`,
-            level: 0
+    prompt.start();
+    console.log(`osu! score finder v${config.version}`);
+    await prompt.get(prompt_schema, function (err, result) {
+        if (err) {
+            logs = ({
+                data: err,
+                type: "obj",
+                level: 0
+            });
+            module_index.logging.main(logs)
+        }
+        if (result) {
+            logs = ({
+                data: `Input received: \nMode: ${result.mode}\nUser-ID: ${result.user_id}\nStart Date: ${result.start_date}\nEnd Date: ${result.end_date}\nConverts: ${result.converts}\n`,
+                type: null,
+                level: 3
+            });
+            module_index.logging.main(logs);
+            data.prompt_out = result;
+            main(data);
+        }
+    });
+
+    async function main(data) {
+        data.user = await axios.get(`https://osu.ppy.sh/api/get_user?k=${api_key.osu_key}&u=${data.prompt_out.user_id}&m=${data.prompt_out.mode}&type=id`).then(res => {
+            logs = ({
+                data: res,
+                type: "obj",
+                level: 4
+            });
+            module_index.logging.main(logs)
+        }).catch(err => {
+            logs = ({
+                data: err,
+                type: "obj",
+                level: 0
+            });
+            module_index.logging.main(logs)
         });
-        module_index.logging.main(logs)
-    }
-    if (result) {
-        logs = ({
-            data: `[DEBUG] | ${new Date(Date.now()).toLocaleString()} | Input received: \nMode: ${result.mode}\nUser-ID: ${result.user_id}\nStart Date: ${result.start_date}\nEnd Date: ${result.end_date}\nConverts: ${result.converts}\n`,
-            level: 4
-        });
-        module_index.logging.main(logs)
-    }
-});
+    };
+};
+wrapper();
--- logging.js
+++ logging.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const config = require('./config.json');
+const util = require('util');
 
 exports.main = async function (logs) {
 
-    switch (config.loglevel) {
+    switch (logs.level) {
         case 0:
             error();
             break;
@@ -23,72 +24,127 @@ exports.main = async function (logs) {
     }
 
     async function error() {
-        if (logs.level > 0) {
+        if (logs.level > config.loglevel) {
             return;
         } else {
-            console.log(logs.data);
+            if (logs.type == "obj"){
+                console.log(`[ERROR] | ${new Date(Date.now()).toLocaleString()} | ${util.inspect(logs.data, {showHidden: false, depth: null})}\n`);
 
-            if (config.logtofile == true) {
-                fs.appendFile('logs.txt', `${logs.data}\n`, function (err) {
-                    if (err) throw err;
-                });
-            };
+                if (config.logtofile == true) {
+                    fs.appendFile('logs.txt', `[ERROR] | ${new Date(Date.now()).toLocaleString()} | ${util.inspect(logs.data, {showHidden: false, depth: null})}\n`, function (err) {
+                        if (err) throw err;
+                    });
+                };
+            } else {
+                console.log(`[ERROR] | ${new Date(Date.now()).toLocaleString()} | ${logs.data}\n`);
+
+                if (config.logtofile == true) {
+                    fs.appendFile('logs.txt', `[ERROR] | ${new Date(Date.now()).toLocaleString()} | ${logs.data}\n`, function (err) {
+                        if (err) throw err;
+                    });
+                };
+            }
+
         }
     };
 
     async function warn() {
-        if (logs.level > 1) {
+        if (logs.level > config.loglevel) {
             return;
         } else {
-            console.log(logs.data);
+            if (logs.type == "obj"){
+                console.log(`[WARN] | ${new Date(Date.now()).toLocaleString()} | ${util.inspect(logs.data, {showHidden: false, depth: null})}\n`);
 
-            if (config.logtofile == true) {
-                fs.appendFile('logs.txt', `${logs.data}\n`, function (err) {
-                    if (err) throw err;
-                });
-            };
+                if (config.logtofile == true) {
+                    fs.appendFile('logs.txt', `[WARN] | ${new Date(Date.now()).toLocaleString()} | ${util.inspect(logs.data, {showHidden: false, depth: null})}\n`, function (err) {
+                        if (err) throw err;
+                    });
+                };
+            } else {
+                console.log(`[WARN] | ${new Date(Date.now()).toLocaleString()} | ${logs.data}\n`);
+
+                if (config.logtofile == true) {
+                    fs.appendFile('logs.txt', `[WARN] | ${new Date(Date.now()).toLocaleString()} | ${logs.data}\n`, function (err) {
+                        if (err) throw err;
+                    });
+                };
+            }
+
         }
     };
 
     async function info() {
-        if (logs.level > 2) {
+        if (logs.level > config.loglevel) {
             return;
         } else {
-            console.log(logs.data);
+            if (logs.type == "obj"){
+                console.log(`[INFO] | ${new Date(Date.now()).toLocaleString()} | ${util.inspect(logs.data, {showHidden: false, depth: null})}\n`);
 
-            if (config.logtofile == true) {
-                fs.appendFile('logs.txt', `${logs.data}\n`, function (err) {
-                    if (err) throw err;
-                });
-            };
+                if (config.logtofile == true) {
+                    fs.appendFile('logs.txt', `[INFO] | ${new Date(Date.now()).toLocaleString()} | ${util.inspect(logs.data, {showHidden: false, depth: null})}\n`, function (err) {
+                        if (err) throw err;
+                    });
+                };
+            } else {
+                console.log(`[INFO] | ${new Date(Date.now()).toLocaleString()} | ${logs.data}\n`);
+
+                if (config.logtofile == true) {
+                    fs.appendFile('logs.txt', `[INFO] | ${new Date(Date.now()).toLocaleString()} | ${logs.data}\n`, function (err) {
+                        if (err) throw err;
+                    });
+                };
+            }
+
         }
     };
 
     async function verbose() {
-        if (logs.level > 3) {
+        if (logs.level > config.loglevel) {
             return;
         } else {
-            console.log(logs.data);
+            if (logs.type == "obj"){
+                console.log(`[VERBOSE] | ${new Date(Date.now()).toLocaleString()} | ${util.inspect(logs.data, {showHidden: false, depth: null})}\n`);
 
-            if (config.logtofile == true) {
-                fs.appendFile('logs.txt', `${logs.data}\n`, function (err) {
-                    if (err) throw err;
-                });
-            };
+                if (config.logtofile == true) {
+                    fs.appendFile('logs.txt', `[VERBOSE] | ${new Date(Date.now()).toLocaleString()} | ${util.inspect(logs.data, {showHidden: false, depth: null})}\n`, function (err) {
+                        if (err) throw err;
+                    });
+                };
+            } else {
+                console.log(`[VERBOSE] | ${new Date(Date.now()).toLocaleString()} | ${logs.data}\n`);
+
+                if (config.logtofile == true) {
+                    fs.appendFile('logs.txt', `[VERBOSE] | ${new Date(Date.now()).toLocaleString()} | ${logs.data}\n`, function (err) {
+                        if (err) throw err;
+                    });
+                };
+            }
+
         }
     };
 
     async function debug() {
-        if (logs.level > 4) {
+        if (logs.level > config.loglevel) {
             return;
         } else {
-            console.log(logs.data);
+            if (logs.type == "obj"){
+                console.log(`[DEBUG] | ${new Date(Date.now()).toLocaleString()} | ${util.inspect(logs.data, {showHidden: false, depth: null})}\n`);
 
-            if (config.logtofile == true) {
-                fs.appendFile('logs.txt', `${logs.data}\n`, function (err) {
-                    if (err) throw err;
-                });
-            };
+                if (config.logtofile == true) {
+                    fs.appendFile('logs.txt', `[DEBUG] | ${new Date(Date.now()).toLocaleString()} | ${util.inspect(logs.data, {showHidden: false, depth: null})}\n`, function (err) {
+                        if (err) throw err;
+                    });
+                };
+            } else {
+                console.log(`[DEBUG] | ${new Date(Date.now()).toLocaleString()} | ${logs.data}\n`);
+
+                if (config.logtofile == true) {
+                    fs.appendFile('logs.txt', `[DEBUG] | ${new Date(Date.now()).toLocaleString()} | ${logs.data}\n`, function (err) {
+                        if (err) throw err;
+                    });
+                };
+            }
+
         }
     };
 }
