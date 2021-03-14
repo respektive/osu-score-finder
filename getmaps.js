85c931d83f9fb39733867b38ba0e2d9ebe5be1ec
Christen Tautz <chrisneuss01@gmail.com>
2021-03-14 17:39:10 +0100

    Fixed Issue #3 * Beta Fix, monitor * Fixed that qualified maps are included * will be customizable in the future * This probably is unstable AF, dont get your hopes up. Code is even more of a mess now :)

--- getmaps.js
+++ getmaps.js
@@ -5,6 +5,7 @@ const config = require('./config.json');
 let maps = { "data": [] };
 let map_data = null;
 let clear = 0;
+let last_req;
 
 async function main() {
 
@@ -12,6 +13,7 @@ async function main() {
         while (map_data == null || map_data.data.length != 0 && clear == 0) {
             console.log(`Requested ${maps.data.length} Maps so far!`)
             async function run() {
+
                 map_data = await axios
                     .get(
                         `https://osu.ppy.sh/api/get_beatmaps?k=${config.apikey}&since=${config.last_map}&m=${config.mode}&a=0`
@@ -20,20 +22,45 @@ async function main() {
                         console.log(err);
                     });
 
+                last_req = map_data.data[map_data.data.length - 1];
+
                 map_data.data.forEach(map => {
-                    if (new Date(map.approved_date) > new Date(config.final_date)) {
+                    if (new Date(map.approved_date) > new Date(config.final_map)) {
                         clear = 1;
+                    } else if (map.beatmapset_id == last_req.beatmapset_id) {
+                        return;
+                    } else if (map.approved != 1 && map.approved != 2 /*&& map.approved != 4*/) {
+                        return;
                     } else {
                         maps.data.push(map);
                     }
-                })
+                });
+
+                map_data = await axios
+                    .get(`https://osu.ppy.sh/api/get_beatmaps?k=${config.apikey}&m=${config.mode}&a=0&s=${last_req.beatmapset_id}`)
+                    .catch((err) => {
+                        console.log(err);
+                    });
+
+                map_data.data.forEach(map => {
+                    if (map.beatmap_id == config.beatmap_id) {
+                        clear = 1;
+                    } else if (map.approved != 1 && map.approved != 2/* && map.approved != 4*/) {
+                        config.beatmap_id = map.beatmap_id
+                    } else if (map.beatmapset_id == last_req.beatmapset_id) {
+                    } else {
+                        maps.data.push(map);
+                    };
+
+                    config.beatmap_id = map.beatmap_id
+
+                });
 
                 config.last_map = maps.data[maps.data.length - 1].approved_date;
                 console.log(config.last_map);
 
                 return (function () { });
             } setTimeout(await run(), 200);
-
         }
     } catch (e) {
         console.log(e);
@@ -46,6 +73,7 @@ async function main() {
         } finally {
             console.log("Finished! DO NOT OPEN IN A SHITTY TEXT EDITOR UNLESS YOU LOVE LAG!");
         }
+
     }
 };
 main();
